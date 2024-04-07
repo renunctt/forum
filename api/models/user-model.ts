2f372a51c0b0cd65c4cecd593f4ab2129b797a2e
renunctt <sultanabibollaev@gmail.com>
2024-04-07 20:51:00 +0500

    refactor user model

--- api/models/user-model.ts
+++ api/models/user-model.ts
@@ -1,5 +1,5 @@
-import { DataTypes } from "sequelize";
-import db from "../util/database";
+import { DataTypes } from 'sequelize'
+import db from '../util/database'
 
 const UserModel = db.define('user', {
   id: {
@@ -10,16 +10,16 @@ const UserModel = db.define('user', {
   },
   name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   password: {
     type: DataTypes.STRING,
-    allowNull: false
-  }
+    allowNull: false,
+  },
 })
 
 export default UserModel
