e667b14721094d6468a9e95ba45ebd0ef3bd3fb1
renunctt <sultanabibollaev@gmail.com>
2024-04-07 22:09:32 +0500

    update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import db from './util/database'
+import router from './router'
 import errorMiddleware from './middlewares/error-middleware'
 
 const PORT = process.env.PORT || 3000
@@ -9,17 +10,24 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
-app.get('/', (_, res) => {
-  res.send('test')
-})
+app.use(
+  cors({
+    credentials: true,
+  }),
+)
+app.use('/api', router)
 app.use(errorMiddleware)
 
-db.sync()
-  .then((_) => {
+const start = async () => {
+  try {
+    await db.sync()
     console.log('Database connected')
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`)
     })
-  })
-  .catch((err) => console.log(err))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
