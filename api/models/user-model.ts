4da53f9606f35e929daeb89c2961ee31201e6c56
renunctt <sultanabibollaev@gmail.com>
2024-04-07 20:41:19 +0500

    add user model

---
+++ api/models/user-model.ts
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import db from "../util/database";
+
+const UserModel = db.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
+
+export default UserModel
