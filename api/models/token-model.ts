8b6cd6c4af7c8d6bc307a7836f3d9833920d7ec0
renunctt <sultanabibollaev@gmail.com>
2024-04-07 22:12:06 +0500

    update user & token model

--- api/models/token-model.ts
+++ api/models/token-model.ts
@@ -1,25 +1,29 @@
 import { DataTypes } from 'sequelize'
 import db from '../util/database'
 
-const TokenModel = db.define('user', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'user',
-      key: 'id',
+const TokenModel = db.define(
+  'token',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    refreshToken: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
   },
-  refreshToken: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-})
+  { tableName: 'token' },
+)
 
 export default TokenModel
--- api/models/user-model.ts
+++ api/models/user-model.ts
@@ -1,25 +1,31 @@
 import { DataTypes } from 'sequelize'
 import db from '../util/database'
 
-const UserModel = db.define('user', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const UserModel = db.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-})
+  { tableName: 'user' },
+)
 
 export default UserModel
