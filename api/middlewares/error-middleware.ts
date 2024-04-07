a706e163c8d840780cb2daa9f1e4f7c7ed4a5a39
renunctt <sultanabibollaev@gmail.com>
2024-04-07 22:13:40 +0500

    fix not work error middleware

--- api/middlewares/error-middleware.ts
+++ api/middlewares/error-middleware.ts
@@ -1,9 +1,10 @@
 import { ApiError } from '../exeptions/api-error'
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
 
-export default function (err: Error, _: Request, res: Response) {
+export default function (err: Error, _: Request, res: Response, next: NextFunction) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message, errors: err.errors })
   }
   return res.status(500).json({ message: 'Непредвиденная ошибка' })
 }
+
